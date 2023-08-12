957f4d2c328b73ce8e57f12b31415421542c334a
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-12 13:26:33 -0400

    Co-authored-by: AndrewSasmito <AndrewSasmito@users.noreply.github.com> Co-authored-by: PlumpyPlump <PlumpyPlump@users.noreply.github.com>

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import './main.css'
+const Title = () => {
+    return (
+        <div>
+            
+            
+            <h1>My Calendar</h1>
+        </div>
+    )
+}
+
+export default Title
+
+
+
---
+++ src/components/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <div><nav>
+        <ul>
+            <li>
+                <a>My Profile</a>
+            </li>
+            <li>
+                <a>My Calendar</a>
+            </li>
+            <li>
+                <a>My Messages</a>
+            </li>
+        </ul>
+    </nav></div>
+    )
+}
+
+export default Navbar
